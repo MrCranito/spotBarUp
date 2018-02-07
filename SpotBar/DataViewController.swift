73c7a2b76241d320d9b6e2b3fffb7e326b22d9ee
Victor moreau <victormoreau@macbook-pro-de-victor.local>
2018-02-07 09:36:03 +0100

    Initial Commit

---
+++ SpotBar/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  SpotBar
+//
+//  Created by Victor moreau on 07/02/2018.
+//  Copyright © 2018 Victor moreau. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
